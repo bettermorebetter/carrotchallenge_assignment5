399d26ef1d39974d4b71b16271dca721cb121e16
bettermorebetter <mingyukim1827@gmail.com>
2023-09-07 04:27:44 +0900

    first commit

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,128 +2,120 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-pink-100 px-12 py-4">
-      <div className="flex justify-center w-full h-5/6 space-x-8">
-        <div className="px-4 py-2 w-1/4 h-full bg-white">
-          {/* Weather title */}
-          <div className="pt-8">
-            <p className="text-4xl font-bold">Weather</p>
+    <div class="flex justify-center items-center w-screen h-screen bg-pink-100 px-12 py-4">
+      <div class="flex justify-center w-full h-5/6 space-x-8">
+        <div class="px-4 py-2 w-1/4 h-full bg-white">
+          <div class="pt-8">
+            <p class="text-4xl font-bold">Weather</p>
           </div>
-          {/* weather1~4 */}
           <div
-            className="px-4 py-4 border-2
+            class="px-4 py-4 border-2
             border-gray-600 rounded-md
             shadow-md shadow-black
           flex justify-between my-4"
           >
             <div>
-              <p className="font-bold">Casius</p>
-              <p className="text-xs">Mars, 12AM</p>
+              <p class="font-bold">Casius</p>
+              <p class="text-xs">Mars, 12AM</p>
             </div>
-            <div className="flex items-center space-x-4">
-              <div className="w-8 h-8 bg-slate-300 rounded-full"></div>
-              <p className="text-3xl font-bold">85</p>
+            <div class="flex items-center space-x-4">
+              <div class="w-8 h-8 bg-slate-300 rounded-full"></div>
+              <p class="text-3xl font-bold">85</p>
             </div>
           </div>
           <div
-            className="px-4 py-4 border-2
+            class="px-4 py-4 border-2
             border-gray-600 rounded-md
             shadow-md shadow-black
           flex justify-between my-4
           bg-yellow-400"
           >
             <div>
-              <p className="font-bold">Dlacria</p>
-              <p className="text-xs">Mars, 12AM</p>
+              <p class="font-bold">Dlacria</p>
+              <p class="text-xs">Mars, 12AM</p>
             </div>
-            <div className="flex items-center space-x-4">
-              <div className="w-8 h-8 bg-slate-300 rounded-full"></div>
-              <p className="text-3xl font-bold">85</p>
+            <div class="flex items-center space-x-4">
+              <div class="w-8 h-8 bg-slate-300 rounded-full"></div>
+              <p class="text-3xl font-bold">85</p>
             </div>
           </div>
           <div
-            className="px-4 py-4 border-2
+            class="px-4 py-4 border-2
             border-gray-600 rounded-md
             shadow-md shadow-black
           flex justify-between my-4
           bg-emerald-400"
           >
             <div>
-              <p className="font-bold">New York</p>
-              <p className="text-xs">USA, 12AM</p>
+              <p class="font-bold">New York</p>
+              <p class="text-xs">USA, 12AM</p>
             </div>
-            <div className="flex items-center space-x-4">
-              <div className="w-8 h-8 bg-slate-300 rounded-full"></div>
-              <p className="text-3xl font-bold">85</p>
+            <div class="flex items-center space-x-4">
+              <div class="w-8 h-8 bg-slate-300 rounded-full"></div>
+              <p class="text-3xl font-bold">85</p>
             </div>
           </div>
           <div
-            className="px-4 py-4 border-2
+            class="px-4 py-4 border-2
             border-gray-600 rounded-md
             shadow-md shadow-black
           flex justify-between my-4
           bg-red-400"
           >
             <div>
-              <p className="font-bold">Zomato</p>
-              <p className="text-xs">India, 12AM</p>
+              <p class="font-bold">Zomato</p>
+              <p class="text-xs">India, 12AM</p>
             </div>
-            <div className="flex items-center space-x-4">
-              <div className="w-8 h-8 bg-slate-300 rounded-full"></div>
-              <p className="text-3xl font-bold">20</p>
+            <div class="flex items-center space-x-4">
+              <div class="w-8 h-8 bg-slate-300 rounded-full"></div>
+              <p class="text-3xl font-bold">20</p>
             </div>
           </div>
-          {/* Plus Button */}
           <div
-            className="shodow-black shadow-lg
+            class="shodow-black shadow-lg
           w-12 h-12 rounded-full
           flex items-center justify-center
           border-2 border-black mx-auto mt-10"
           >
-            <p className="text-3xl">+</p>
+            <p class="text-3xl">+</p>
           </div>
         </div>
         <div
-          className="w-1/4 bg-white flex
+          class="w-1/4 bg-white flex
         flex-col items-center space-y-4"
         >
-          {/* X Button */}
-          <div className="px-8 w-full mt-8 flex justify-start">
+          <div class="px-8 w-full mt-8 flex justify-start">
             <div
-              className="flex items-center
+              class="flex items-center
             justify-center border-2 border-black
             rounded-full w-8 h-8"
             >
               <p>X</p>
             </div>
           </div>
-          {/* Simple Tag(subtitle) */}
           <div>
-            <p className="text-sm font-semibold">SIMPLE TAG</p>
+            <p class="text-sm font-semibold">SIMPLE TAG</p>
           </div>
-          {/* Title */}
-          <div className="flex">
+          <div class="flex">
             <p
-              className="text-2xl text-center
+              class="text-2xl text-center
             font-bold"
             >
               Work with best designers
             </p>
           </div>
-          {/* images */}
           <div
-            className="w-5/6 h-2/5 
+            class="w-5/6 h-2/5 
           grid grid-rows-2 grid-cols-2
           content-center gap-6"
           >
-            <div className="bg-gray-400"></div>
-            <div className="bg-gray-400"></div>
-            <div className="bg-gray-400"></div>
-            <div className="bg-gray-400"></div>
+            <div class="bg-gray-400"></div>
+            <div class="bg-gray-400"></div>
+            <div class="bg-gray-400"></div>
+            <div class="bg-gray-400"></div>
           </div>
-          {/* complete button */}
           <div
-            className="border-2 bg-sky-500
+            class="border-2 bg-sky-500
           w-4/5 flex justify-center items-center
           rounded-md p-2"
           >
@@ -131,22 +123,21 @@ const Home: NextPage = () => {
           </div>
         </div>
         <div
-          className="w-1/4 bg-yellow-300
+          class="w-1/4 bg-yellow-300
         space-y-2 flex flex-col items-center
         px-4"
         >
-          {/* back button&title div */}
           <div
-            className="py-8 flex items-center
+            class="py-8 flex items-center
           justify-start w-full px-4"
           >
             <div
-              className="flex items-center
+              class="flex items-center
             justify-center border-2 border-black
             rounded-full w-8 h-8 bg-white"
             >
               <svg
-                className="w-4 h-4"
+                class="w-4 h-4"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -160,38 +151,33 @@ const Home: NextPage = () => {
                 />
               </svg>
             </div>
-            <p className="ml-16">Friends</p>
+            <p class="ml-16">Friends</p>
           </div>
-          {/* search div */}
           <div
-            className="border-2 border-black
+            class="border-2 border-black
           rounded-md py-2 w-full px-4 bg-white"
           >
-            <input
-              className="bg-transparent"
-              placeholder="Search with love ..."
-            />
+            <input class="bg-transparent" placeholder="Search with love ..." />
           </div>
-          {/* friends. */}
           {[1, 1, 1].map((_, i) => (
             <div
               key={i}
-              className="border-2
+              class="border-2
             border-black w-full px-4 py-2
             flex items-center rounded-md
             space-x-4 bg-white"
             >
               <div
-                className="w-8 h-8 rounded-full
+                class="w-8 h-8 rounded-full
               bg-slate-400"
               ></div>
               <div>
-                <p className="text-md font-bold">Bill Rizer</p>
-                <p className="text-xs text-gray-300">Planet Designer</p>
+                <p class="text-md font-bold">Bill Rizer</p>
+                <p class="text-xs text-gray-300">Planet Designer</p>
               </div>
               <div>
                 <div
-                  className="border-2 border-black 
+                  class="border-2 border-black 
                 bg-yellow-400 text-sm rounded-md
                 px-2"
                 >
